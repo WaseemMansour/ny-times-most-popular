18a7ec3fd38d2a100df97ae082c1530d82599773
Waseem Mansour <waseem.mn2002@gmail.com>
2024-03-15 06:06:48 +0400

    chore: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import NyTimesLogo from '@/assets/new-york-times-logo.svg';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Router from './router';
+
+const queryClient = new QueryClient()
 
 function App() {
 
   return (
-    <>
-      <h1>NY Times</h1>
-    </>
+    <div>
+        <h1>
+        <img
+            src={NyTimesLogo}
+            alt="The New York Times Logo"
+            width={50}
+            height={40}
+          />
+          NY Times
+        </h1>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
+    </div>
   )
 }
 
---
+++ src/router/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import MostPopularArticles from '../modules/articles/pages/MostPopularArticles';
+import Layout from '../modules/core/components/Layout';
+import PageNotFound from '../modules/core/pages/NotFound';
+
+const ArticleDetails = React.lazy(() => import('../modules/articles/pages/ArticleDetails'));
+
+function Router() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MostPopularArticles />} />
+        <Route
+          path="articles/:id"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <ArticleDetails />
+            </React.Suspense>
+          }
+        />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default Router
