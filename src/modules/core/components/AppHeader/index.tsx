5437e428cb5afb498a9643e1c41e0adf6d72654e
Waseem Mansour <waseem.mn2002@gmail.com>
2024-03-15 17:07:45 +0400

    chore: connect logo with main route

--- src/modules/core/components/AppHeader/index.tsx
+++ src/modules/core/components/AppHeader/index.tsx
@@ -1,19 +1,24 @@
 import { Flex, Heading } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 import NyTimesLogo from '../../../../assets/new-york-times-logo.svg';
 
 const AppHeader = () => {
   return (
-  <Flex alignItems={'center'} margin='2rem 0' justifyContent={'center'}>
-    <img
-      src={NyTimesLogo}
-      alt="The New York Times Logo"
-      width={50}
-      height={40}
-    />
-    <Heading as='h1' fontWeight={500} color='#000' marginLeft='1rem'>
-      The New York Times
-    </Heading>
-  </Flex>
+  
+    <Link to='/'>
+      <Flex alignItems={'center'} margin='2rem 0' justifyContent={'center'}>
+      <img
+        src={NyTimesLogo}
+        alt="The New York Times Logo"
+        width={50}
+        height={40}
+      />
+      <Heading as='h1' fontWeight={500} color='#000' marginLeft='1rem'>
+        The New York Times
+      </Heading>
+      </Flex>
+    </Link>
+  
   );
 }
 
